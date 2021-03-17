b11efb9b7eb1d1e4b2cacf543a0a741a5c46d70b
Yaroslav Hamuha <yhamuha@gmail.com>
2021-03-16 19:10:14 -0700

    variables

---
+++ variables.py
@@ -0,0 +1,16 @@
+first_name = (' hi my name is Anton')
+last_name = ('Hamuha')
+print(first_name + last_name)
+age = 'and im 9 years old' 
+print(age)
+name = 'I like games'
+print(name)
+Family = 'i have a brother a mom and a dad'
+print(Family)
+Family2 = 'I also have 2 cousins and a auntie and a aunt'
+print(Family2)
+Favorite_movie = "my favorite movie is Monsters university"
+print(Favorite_movie)
+name = 'and i am learning how to code in Python!'
+print(name)
+
