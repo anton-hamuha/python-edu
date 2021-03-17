6b4efe87cb3c9060f899c922dbe65f5053c7a28c
Anton Hamuha <anton.hamuha@gmail.com>
2021-03-17 12:59:15 -0700

    return if-else and commit input

--- ifelse.py
+++ ifelse.py
@@ -1,4 +1,7 @@
-print("what is you favorite food?")
-answer = input ("food:")
-print("what is your name?")
-answer = input ("name:")
+age = 8
+if age == 15:
+    print ("I'm 15")
+elif age > 20:
+    print ("I'm over 20")
+else:
+    print ("I'm not 15 and I'm not over 20")
---
+++ input.py
@@ -0,0 +1,4 @@
+print("what is you favorite food?")
+answer = input ("food:")
+print("what is your name?")
+answer = input ("name:")
