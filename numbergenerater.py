d8420657e9ad93722dfe90f8c9f44060e5bfe3d5
Anton Hamuha <anton.hamuha@gmail.com>
2021-03-22 14:32:02 -0700

    random number generater

---
+++ numbergenerater.py
@@ -0,0 +1,11 @@
+import random
+
+x = int(input("Type any number to receive a random\none back - "))
+
+
+def random_num():
+    if 0 == 0:
+        print(x + random.randint(0, 1000))
+
+
+print(random_num())
