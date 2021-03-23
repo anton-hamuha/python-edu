828867e65a3e6e39bbeef2ef862a6269e46c7d15
Anton Hamuha <anton.hamuha@gmail.com>
2021-03-23 13:05:04 -0700

    black jack: Create lists for the deck of cards, and for the player and dealer hands

---
+++ blackjack.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  6 20:45:35 2020
+
+@author: Dylan
+"""
+import random
+import time
+
+deck = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+player_hand = []
+dealer_hand = []
+
+
+
+
+
+
+
