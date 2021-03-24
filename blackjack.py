3f5651fb23d4ae2850d7be31b16a4ab03b5797db
Anton Hamuha <anton.hamuha@gmail.com>
2021-03-24 10:05:28 -0700

    black jack:Use the random library to randomly select cards to deal and add them to the player and dealer hands.

--- blackjack.py
+++ blackjack.py
@@ -10,10 +10,6 @@ import time
 deck = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 player_hand = []
 dealer_hand = []
-
-
-
-
-
-
-
+import random
+card = random.choice(cards)
+player_hand.append(card)
