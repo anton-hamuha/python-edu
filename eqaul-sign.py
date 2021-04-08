b552bcd0cfc87a5b1c2d1a9d8c9a3183cb2dff34
Anton Hamuha <anton.hamuha@gmail.com>
2021-04-07 20:18:59 -0700

    Where the equal sign is used to assign value

---
+++ eqaul-sign.py
@@ -0,0 +1,10 @@
+>>> Item_name = "Computer" #A String
+>>> Item_qty = 10 #An Integer
+>>> Item_value = 1000.23 #A floating point
+>>> print(Item_name)
+Computer
+>>> print(Item_qty)
+10
+>>> print(Item_value)
+1000.23
+>>>
