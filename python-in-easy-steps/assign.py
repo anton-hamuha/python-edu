db1c0a8a3ed4c965b226641349144ef7ba40385a
Anton Hamuha <anton.hamuha@gmail.com>
2021-09-08 17:04:12 -0700

    Assigning Values

---
+++ python-in-easy-steps/assign.py
@@ -0,0 +1,9 @@
+a = 8
+b = 4
+print('Assign Values:\t\t','a =',a,'\tb =',b)
+a += b
+print('Add & Assign:\t\t','a =',a,'(8 += 4)')
+a -= b
+print('Subtract & Assign:\t','a =',a,'(12 - 4)')
+a *= b
+print('Multiply & Assign:\t','a =',a,'(8 x 4)')
