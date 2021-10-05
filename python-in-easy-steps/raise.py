081e9eb6208a12721f946b54b94c5840735fd182
Anton Hamuha <anton.hamuha@gmail.com>
2021-10-05 16:46:21 -0700

    handling exceptations

---
+++ python-in-easy-steps/raise.py
@@ -0,0 +1,8 @@
+day = 32
+try:
+    if day > 31:
+        raise value error('invalid day number')
+except valueerror as msg:
+        print('the program found an', msg)
+finally:
+    print('but today is a beautiful anyway.')
---
+++ python-in-easy-steps/try.py
@@ -0,0 +1,5 @@
+title = 'python in easy steps'
+try:
+    print(title)
+except nameError as msg:
+    print(msg)
