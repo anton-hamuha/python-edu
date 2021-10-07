e8f3cdeaa033fc5771911bb3bcdbcd7826ff6fb6
Anton Hamuha <anton.hamuha@gmail.com>
2021-10-07 16:47:50 -0700

    storing functions

---
+++ python-in-easy-steps/cat.py
@@ -0,0 +1,6 @@
+def purr(pet = 'a cat'):
+    print(pet, 'says MEOW!')
+ def lick(pet = 'a cat'):
+     print(pet, 'drinks milk')
+def nap(pet = 'a cat'):
+    print(pet, 'sleeps by the fire')
---
+++ python-in-easy-steps/tiger.py
@@ -0,0 +1,5 @@
+import cat
+pet = input('enter a pet name:')
+cat.purr(pet)
+cat.lick(pet)
+cat.nap(pet)
