27949725b013c20464a15b52b6b260fe892e32ec
Anton Hamuha <anton.hamuha@gmail.com>
2021-10-12 16:32:34 -0700

    performing mathematics

---
+++ python-in-easy-steps/maths.py
@@ -0,0 +1,8 @@
+import math, random
+print('rounding up 9.5:', math.ceil(9.5))
+print('rounding up 9.5:', math.floor(9.5))
+num = 4
+print(num, 'squared:', math.pow(num, 2))
+print(num, 'square root:', math.sqrt(num))
+nums = random.sample(range(1, 49), 6)
+print('your lucky lotto numbers are:', nums)
