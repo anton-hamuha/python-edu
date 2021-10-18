6f8b66aebea85e622f2d4ed815ba7e9534eab1a8
Anton Hamuha <anton.hamuha@gmail.com>
2021-10-18 09:57:22 -0700

    manipulating strings

---
+++ python-in-easy-steps/manipulate.py
@@ -0,0 +1,8 @@
+def display(s):
+    print(s)
+display(display._doc_)
+display(r'c:\program files')
+display('\nhello' + 'python')
+display('python in easy steps\n'[7:])
+display('p' in 'python')
+display('p' in 'python')
