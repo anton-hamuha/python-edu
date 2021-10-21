2891b66f90cd5fd76a673c00b0c97f7607d63ff8
Anton Hamuha <anton.hamuha@gmail.com>
2021-10-21 16:11:56 -0700

    modifying strings

---
+++ python-in-easy-steps/modify.py
@@ -0,0 +1,8 @@
+string = 'python in easy steps'
+print('\ncapitalized:\t', string.capitalize())
+print('\ntitled:\t\t', string.title())
+print('\ncentered:\t', string.center(30, '*'))
+print('\nuppercase:\t', string.upper())
+print('\njoined:\t\t', string.join('**'))
+print('\njustified:\t', string.rjust(30, '*'))
+print('\nreplaced:\t', string.replace('s', '*'))
